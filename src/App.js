803b3bb2ecb7a1c758ce42162917e8677e5e5ef7
Joern Bergmeier <joern@webcoffee.io>
2023-10-19 21:49:23 +0200

    added new text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='subtext'>
+          coming soon...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='email'>
+          <a href="mailto:hello@local-friends-hamm.de?subject=Hello">hello@local-friends-hamm.de</a>
+        </p>
       </header>
     </div>
   );
